dd706fffb7afe7d9c8c181c29d3e93fd4841f60f
Official-Kallol <official.kallol2024@gmail.com>
2025-08-01 12:28:42 +0530

    feat: Add Navbar, ResumeCard, and ScoreCircle components
    - Implemented Navbar for navigation with links to home and upload sections.
    - Created ResumeCard component to display individual resume details with a link to view more.
    - Developed ScoreCircle component to visually represent feedback scores with a circular progress indicator.
    - Added sample resumes data in constants for rendering in the home page.
    - Updated home route to include Navbar and display a list of ResumeCard components.
    - Removed unused logo assets and welcome component as part of the new structure.
    - Defined Resume and Feedback interfaces in TypeScript for better type safety.

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <Link to= "/">
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+        </Link>
+        <Link to= "/upload">
+        <button className="primary-button w-fit">
+            Upload Resume
+            </button>
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
